1257ef2e6ecb05b8e3725181ea3c8f10b8fca42b
tdascoli <R0hrch@ch>
2021-10-04 22:07:14 +0200

    add bootstrap as fallback

--- src/php/includes/style.cntnd_simple_booking_output.php
+++ src/php/includes/style.cntnd_simple_booking_output.php
@@ -1,3 +1,4 @@
 <style>
-<?= file_get_contents($cfgClient[$client]["module"]["path"].'cntnd_simple_booking/css/cntnd_simple_booking.css') ?>
+    @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css');
+    <?= file_get_contents($cfgClient[$client]["module"]["path"].'cntnd_simple_booking/css/cntnd_simple_booking.css') ?>
 </style>
