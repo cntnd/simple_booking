e8683fc332e57770a7ce81fe20d3cd3a6ece4b5b
Thomas D'Ascoli <thomas@dasco.li>
2025-01-31 10:31:36 +0100

    Update class.cntnd_simple_booking.php

--- src/php/includes/class.cntnd_simple_booking.php
+++ src/php/includes/class.cntnd_simple_booking.php
@@ -168,15 +168,14 @@ class CntndSimpleBooking
 
     private function reccurentIndexByWeekday($weekday)
     {
-        $index_date = DateTimeUtil::getIndexFromWeekday($weekday);
-        $index = DateTimeUtil::getIndexFromDate(DateTimeUtil::getDateFromDaterange($this->daterange, $index_date));
+        $index = DateTimeUtil::getIndexFromDate(DateTimeUtil::getDateFromDaterange($this->daterange, $weekday));
         return $index;
     }
 
     private function recurrentConfig()
     {
         $config = $this->config();
-
+		
         foreach ($this->blocked_days as $day => $blocked) {
             if (!$blocked) {
                 $index = $this->reccurentIndexByWeekday($day);
@@ -898,7 +897,7 @@ class CntndSimpleBooking
     public function payments()
     {
         // bookings
-        $sql = "SELECT * FROM :table WHERE idart = :idart ORDER BY date, time";
+        $sql = "SELECT * FROM :table WHERE idart = :idart ORDER BY date ASC, time";
         $values = array(
             'table' => self::$_vars['db']['bookings'],
             'idart' => cSecurity::toInteger($this->idart));
@@ -907,13 +906,14 @@ class CntndSimpleBooking
         while ($this->db->next_record()) {
             $readableDate = DateTimeUtil::getReadableDate($this->db->f('date'));
             $readableTime = DateTimeUtil::getReadableTimeFromDate($this->db->f('time'));
+			$index = DateTimeUtil::getFullIndexFromDateTime($this->db->f('time'));
             $title = $readableDate . " - " . $readableTime;
-            $bookings[$this->db->f('id')] = ["title" => $title];
+            $bookings[$this->db->f('id')] = ["title" => $title, "index" => $index];
         }
 
         // payments
         $references = implode(",", array_keys($bookings));
-        $sql = "SELECT * FROM :table WHERE reference_id IN (:references) ORDER BY id";
+        $sql = "SELECT * FROM :table WHERE reference_id IN (:references)";
         $values = array(
             'table' => self::$_vars['db']['payment'],
             'references' => $references);
@@ -922,7 +922,7 @@ class CntndSimpleBooking
         if ($result->num_rows > 0) {
             while ($this->db->nextRecord()) {
                 $referenceId = $this->db->f('reference_id');
-                $payments[] = [
+                $payments[$bookings[$referenceId]["index"]] = [
                     "title" => $bookings[$referenceId]["title"],
                     "name" => $this->db->f('forename') . " " . $this->db->f('surname'),
                     "email" => $this->db->f('email'),
@@ -933,6 +933,7 @@ class CntndSimpleBooking
                 ];
             }
         }
+		krsort($payments);
         return $payments;
     }
 
